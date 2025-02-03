37d229d5ebd0b957576ad69e7242566ecd365bf1
Golagani Rashmika <golagani.rashmika@accenture.com>
2025-02-03 13:12:40 +0530

    created 2 components App and counter

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./App.css";
+export default function Counter() {
+  return (
+    <div>
+      <button>Generate</button>
+      <button>Reset</button>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>App</h1>
+    </div>
+  );
+}

1020ec93d3d8e6107a2e7ca1fa40c1b34ca92019
Golagani Rashmika <golagani.rashmika@accenture.com>
2025-02-03 21:34:55 +0530

    created classes and updated the counterdisplay

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-function Counter() {
+export default function Counter() {
   const [count, setCount] = useState(0);
   function increment() {
     setCount(count + 1);
@@ -21,24 +21,27 @@ function Counter() {
   );
 }
 function CountDisplay({ value }) {
-  return (
-    <div>
-      <span
-        style={{
-          color: "blue",
-          fontSize: "64px",
-        }}
-      >
-        {value}
-      </span>
-    </div>
-  );
-}
+  let imagesrc;
 
-export default function App() {
+  if (value < 50) {
+    imagesrc =
+      "https://i.pinimg.com/736x/b0/f7/f8/b0f7f8165f2dda408cde0730a2f08c23.jpg";
+  } else if (value < 100) {
+    imagesrc =
+      "https://media.istockphoto.com/id/1388186992/vector/yellow-happy-face-vector-symbol-icon.jpg?s=612x612&w=0&k=20&c=GZBWkt0ZLxLHkxU58BhRqkk41xC0LySjXa0N65xMDlw=";
+  } else {
+    imagesrc =
+      "https://media.istockphoto.com/id/1408363720/vector/loudly-crying-emoticon.jpg?s=612x612&w=0&k=20&c=oqmC21THjO4uB86BR58opOIkmcBE0n47Bf0QmUdMFcA=";
+  }
+
   return (
-    <div>
-      <Counter />
+    <div className="contanier">
+      <div>
+        <img className="imagestyle" src={imagesrc} alt="image" />
+      </div>
+      <div className="counterdata">
+        <h1>{value}</h1>
+      </div>
     </div>
   );
 }
